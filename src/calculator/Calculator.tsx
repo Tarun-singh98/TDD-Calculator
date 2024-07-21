7e5a5cc1efff0eafd600fda57fc6712405d40cfc
Tarun Singh <ts5755798@gmail.com>
2024-07-21 20:32:00 +0530

    -Updated the Calculator.tsx to handle the occurance of negative numbers

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Calculator = ({ inputString }: { inputString: string }) => {
   const add = (numbers: string): number => {
+    // To handle the empty string
     if (!numbers) {
       return 0;
     }
@@ -16,6 +17,14 @@ const Calculator = ({ inputString }: { inputString: string }) => {
     }
 
     const newNumber = numbers.split(delimiter);
+
+    // To handle the negative numbers
+    const negatives = newNumber.filter((num) => parseInt(num, 10) < 0);
+
+    if (negatives.length) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
+    }
+
     let sum = 0;
     for (let i = 0; i < newNumber.length; i++) {
       sum += parseInt(newNumber[i], 10);
@@ -30,11 +39,7 @@ const Calculator = ({ inputString }: { inputString: string }) => {
     output = error.message;
   }
 
-  return (
-    <div>
-      <div>{output}</div>
-    </div>
-  );
+  return <div>{output}</div>;
 };
 
 export default Calculator;
