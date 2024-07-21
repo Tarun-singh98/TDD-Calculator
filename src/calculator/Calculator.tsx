9682292671dfeac623214c41b038d75a712ea0da
Tarun Singh <ts5755798@gmail.com>
2024-07-21 18:26:32 +0530

    -Updated the code to make the new line character test pass

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -2,19 +2,20 @@ import React from "react";
 
 const Calculator = ({ inputString }: { inputString: string }) => {
   const add = (numbers: string): number => {
-    if (numbers === "") {
+    if (!numbers) {
       return 0;
-    } else if (numbers?.includes(",")) {
-      let number = numbers.split(",");
-      let sum = 0;
-      for (let i = 0; i < number.length; i++) {
-        sum += parseInt(number[i], 10);
-      }
-      return sum;
-    } else {
-      return parseInt(inputString, 10);
     }
+
+    let delimeter = /\n|,/;
+
+    const newNumber = numbers.split(delimeter);
+    let sum = 0;
+    for (let i = 0; i < newNumber.length; i++) {
+      sum += parseInt(newNumber[i], 10);
+    }
+    return sum;
   };
+
   let output: any;
   try {
     output = add(inputString);
