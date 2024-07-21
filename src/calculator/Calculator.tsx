3e6e8e76e4e66875119b8492aeb200a42b8553df
Tarun Singh <ts5755798@gmail.com>
2024-07-21 18:00:45 +0530

    -Changes made in the calculator.tsx regarding the initial 3 tests (Empty String, only one number given as input and 2 numbers separated by ",")

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 
-const Calculator = ( {inputString}: any) => {
+const Calculator = ({ inputString }: { inputString: string }) => {
+  const add = (numbers: string): number => {
+    if (numbers === "") {
+      return 0;
+    } else if (numbers?.includes(",")) {
+      let number = numbers.split(",");
+      let sum = 0;
+      for (let i = 0; i < number.length; i++) {
+        sum += parseInt(number[i], 10);
+      }
+      return sum;
+    } else {
+      return parseInt(inputString, 10);
+    }
+  };
+  let output: any;
+  try {
+    output = add(inputString);
+  } catch (error: any) {
+    output = error.message;
+  }
 
-  const output = 0;
   return (
     <div>
       <div>{output}</div>
