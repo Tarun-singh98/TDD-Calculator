6a3bab46ca5a30a76cffccba32f50e9cd2d3adfd
Tarun Singh <ts5755798@gmail.com>
2024-07-21 20:20:30 +0530

    -Updte the Calculator.tsx to handle different delimiters between numbers

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -6,9 +6,16 @@ const Calculator = ({ inputString }: { inputString: string }) => {
       return 0;
     }
 
-    let delimeter = /\n|,/;
+    let delimiter: any = /\n|,/;
 
-    const newNumber = numbers.split(delimeter);
+    // To handle the different delimiters between numbers
+    if (numbers.startsWith("//")) {
+      const delimiterEndIndex = numbers.indexOf("\n");
+      delimiter = new RegExp(numbers.slice(2, delimiterEndIndex));
+      numbers = numbers.slice(delimiterEndIndex + 1);
+    }
+
+    const newNumber = numbers.split(delimiter);
     let sum = 0;
     for (let i = 0; i < newNumber.length; i++) {
       sum += parseInt(newNumber[i], 10);
